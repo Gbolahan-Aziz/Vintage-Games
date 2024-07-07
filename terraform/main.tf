303008b5c98ec506143fd3c68a8ed9c06a44135b
Gbolahan-Aziz <therealslimzizzy.com>
2024-07-07 09:21:55 +0100

    dependency

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ terraform {
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
   cluster_name    = var.cluster_name
-  cluster_version = "1.21"
+  cluster_version = "1.27"
   subnet_ids      = module.vpc.public_subnets
   vpc_id          = module.vpc.vpc_id
   eks_managed_node_groups = {
