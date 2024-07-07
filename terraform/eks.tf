ba536acad042ca5d934997d71598a6fbd0af5d7a
Gbolahan-Aziz <therealslimzizzy.com>
2024-07-07 07:03:55 +0100

    fix bugs

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -1,38 +1,21 @@
-resource "aws_eks_cluster" "game_cluster" {
-  name     = var.cluster_name
-  role_arn = aws_iam_role.eks_cluster_role.arn
-
-  vpc_config {
-    subnet_ids = module.vpc.public_subnets
-  }
-
-  depends_on = [
-    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.eks_cluster_AmazonEKSVPCResourceController
-  ]
-}
-
 resource "aws_eks_fargate_profile" "fargate_profile" {
-  cluster_name          = aws_eks_cluster.game_cluster.name
+  cluster_name          = var.cluster_name
   fargate_profile_name  = "game-cluster-fargate"
   pod_execution_role_arn = aws_iam_role.eks_pod_execution_role.arn
-  subnet_ids            = module.vpc.public_subnets
+  subnet_ids            = module.vpc.private_subnets
 
   selector {
     namespace = "default"
   }
 
-  depends_on = [
-    aws_eks_cluster.game_cluster
-  ]
 }
 
 data "aws_eks_cluster" "game_cluster" {
-  name = aws_eks_cluster.game_cluster.name
+  name = var.cluster_name
 }
 
 data "aws_eks_cluster_auth" "game_cluster" {
-  name = aws_eks_cluster.game_cluster.name
+  name = var.cluster_name
 }
 
 data "tls_certificate" "eks_oidc_thumbprint" {
