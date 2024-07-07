97f06baca6737461177214b5d223ad22c2a8572b
Gbolahan-Aziz <therealslimzizzy.com>
2024-07-07 10:14:49 +0100

    dependency

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -11,24 +11,3 @@ resource "aws_eks_fargate_profile" "fargate_profile" {
 
 }
 
-data "aws_eks_cluster" "game_cluster" {
-  name = var.cluster_name
-  depends_on =  [module.eks]
-}
-
-data "aws_eks_cluster_auth" "game_cluster" {
-  name = var.cluster_name
-  depends_on =  [module.eks]
-}
-
-data "tls_certificate" "eks_oidc_thumbprint" {
-  url = data.aws_eks_cluster.game_cluster.identity[0].oidc[0].issuer
-  depends_on =  [module.eks]
-}
-
-resource "aws_iam_openid_connect_provider" "oidc_provider" {
-  client_id_list  = ["sts.amazonaws.com"]
-  thumbprint_list = [data.tls_certificate.eks_oidc_thumbprint.certificates[0].sha1_fingerprint]
-  url             = data.aws_eks_cluster.game_cluster.identity[0].oidc[0].issuer
-  depends_on =  [module.eks]
-}
